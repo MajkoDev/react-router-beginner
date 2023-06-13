9eb15bce85f2b5ecc8a48f21ed89829198ee6e94
Marian <hellomariandev@gmail.com>
2023-06-13 20:36:57 +0200

    dynamic routes

---
+++ src/components/UserDetails.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+const UserDetails = () => {
+  return (
+    <div>
+        <h2>Details about User</h2>
+    </div>
+  )
+}
+
+export default UserDetails
