84da0159434583729c8747fa33e4594539c81cc8
Marian <hellomariandev@gmail.com>
2023-06-17 07:18:48 +0200

    dynamic routes

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useParams } from "react-router-dom";
 
 const UserDetails = () => {
+  const { userId } = useParams();
+  // const userId = params.userId
+
   return (
     <div>
-        <h2>Details about User</h2>
+      <h2>Details about User {userId}</h2>
     </div>
-  )
-}
+  );
+};
 
-export default UserDetails
+export default UserDetails;
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,12 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useSearchParams } from "react-router-dom";
 
 const Users = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const showActiveUers = searchParams.get('filter') === 'active'
+
   return (
     <div>
       <h3>User 1</h3>
       <h3>User 2</h3>
       <h3>User 3</h3>
       <Outlet />
+      <div>
+        <button onClick={() => setSearchParams({ filter: "active" })}>Active Users</button>
+        <button onClick={() => setSearchParams({})}>Reset Filter</button>
+      </div>
+      {
+        showActiveUers ? <h2>show active users</h2> : <h2>showing all users</h2>
+      }
     </div>
   );
 };
