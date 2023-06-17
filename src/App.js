c174eb43cce64986ebdc656453d749f81fcc1fa5
Marian <hellomariandev@gmail.com>
2023-06-17 07:15:30 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import "./App.css";
+
 import Home from "./components/Home";
 import About from "./components/About";
-import "./App.css";
+
+import Navbar from "./components/Navbar";
+import OrderSummary from "./components/OrderSummary";
+import NoMatch from "./components/NoMatch";
+
+import Products from "./components/Products";
+import FeaturedProducts from "./components/FeaturedProducts";
+import NewProducts from "./components/NewProducts";
+
+import Users from "./components/Users";
+import UserDetails from "./components/UserDetails";
+import Admin from "./components/Admin";
 
 function App() {
   return (
     <>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="about" element={<About />} />
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="order-summary" element={<OrderSummary />} />
+
+        <Route path="products" element={<Products />}>
+          {/* Index Route - choose which child route should render ar parent level */}
+          <Route index element={<FeaturedProducts />} />
+          {/* Nested Routes */}
+          <Route path="featured" element={<FeaturedProducts />} />
+          <Route path="new" element={<NewProducts />} />
+        </Route>
+
+        {/* Dynamic Routes */}
+        <Route path='users' element={<Users />} >
+          <Route path=':userId' element={<UserDetails />} />
+          <Route path='admin' element={<Admin />} />
+        </Route>
+
+        {/* No Match Route */}
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
     </>
   );
 }
